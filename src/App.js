152fbb19e862e5290a133ca53cf41bec037132c7
Piotr Kaminski <piterwk1@.wp.pl>
2021-01-31 08:56:56 +0100

    implement of app store and request helper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import StoreProvider from './store/StoreProvider';
+
 import './App.scss';
 
-const App = () => <header>Hello kutsay</header>;
+const App = () => (
+  <StoreProvider>
+    <header>Hello kutsay</header>
+  </StoreProvider>
+);
 
 export default App;
