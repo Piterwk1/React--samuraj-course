6d054691bfa3c8ba9871175d7dcc5fc1c54f6917
Piotr Kaminski <piterwk1@.wp.pl>
2021-02-02 18:23:35 +0100

    Navigation of app

---
+++ src/components/AsideMenu/AsideMenu.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules';
+
+import UserMenu from './subcomponents/UserMenu';
+import AdminMenu from './subcomponents/AdminMenu';
+
+import { default as AsideMenuStyle } from './AsideMenu.module.scss';
+import { StoreContext } from '../../store/StoreProvider';
+
+const style = bemCssModules(AsideMenuStyle);
+
+const ADMIN_TYPE = 1;
+
+const AsideMenu = () => {
+  const { user } = useContext(StoreContext);
+
+  const adminMenuComponent =
+    user?.accessLevel === ADMIN_TYPE ? <AdminMenu /> : null;
+
+  return (
+    <section className={style()}>
+      <UserMenu isUserLogged={Boolean(user)} />
+      {adminMenuComponent}
+    </section>
+  );
+};
+
+export default AsideMenu;
